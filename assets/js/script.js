1fea62e0d26fa810bcb28889605138008a560b0f
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-01-11 21:57:34 -0500

    add function that displays current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,53 @@ function getLatLon(cityName) {
     })
     .then(function (data) {
       console.log(data);
-
+      // stored lat and lon data in variables
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+      // obtained current weather by passing it data from cityUrl
+      displayCurrentWeather(data);
+      // obtained future weather by passing it lat,lon from cityUrl
+      displayFutureWeather(lat, lon);
     });
 }
 
+// Used data obtained from cityUrl to generate current weather
+function displayCurrentWeather(data) {
+  // created variables to store all required data to be displayed on the page
+  var cityname = data.name;
+  var cityDate = dayjs().format("dddd, D MMM YYYY");
+  var weatherIcon = data.weather[0].icon;
+  var temperature = data.main.temp;
+  var humidity = data.main.humidity;
+  var windSpeed = data.wind.speed;
+  console.log(windSpeed);
 
+  var cityTitle = $("<h3>");
+  // Date
+  var icon = $("<img>").attr(
+    "src",
+    `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+  );
+  var date = $("<p>");
+  var temp = $("<p>");
+  var wind = $("<p>");
+  var humidityLevel = $("<p>");
+  // added attributes
+  cityTitle.attr("id", "cityTitle").text(cityname);
+  icon.attr("id", "weatherIcon").text(weatherIcon);
+  date.attr("id", "cityDate").text(cityDate);
+  temp.attr("id", "temp").text("Temp: " + temperature);
+  wind.attr("id", "wind-Speed").text("Wind: " + windSpeed);
+  humidityLevel.attr("id", "hum").text("Humidity: " + humidity);
+  // appended children elements to parents elements
+  $("#currentWeather").append(cityTitle);
+  $("#currentWeather").append(cityDate);
+  $("#currentWeather").append(icon);
+  $("#currentWeather").append(temp);
+  $("#currentWeather").append(wind);
+  $("#currentWeather").append(humidityLevel);
+
+  var weatherTitle = $("<h3>");
+  weatherTitle.text("5-Day Forecast");
+  $("#currentWeather").append(weatherTitle);
+}
